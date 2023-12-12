a382eaf8da44ce1a79526e332be329561e2dff1c
jb-edzang <edzangjeanbosco@gmail.com>
2023-12-12 17:51:50 +0100

    first commit

---
+++ nextConfig.js
@@ -0,0 +1,15 @@
+// next.config.js
+module.exports = {
+  /* Vos options de configuration ici */
+
+  // Exemple de configuration de chemins personnalisés
+  basePath: "/ed-editions",
+  assetPrefix: "/ed-editions/",
+
+  // Exemple de configuration des modules CSS
+  // Ce bloc permet d'activer les modules CSS avec Sass
+  // pour les fichiers .scss ou .sass
+  sassOptions: {
+    includePaths: ["./styles"],
+  },
+};
